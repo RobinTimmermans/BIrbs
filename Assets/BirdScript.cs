72a993d265967cc0c3ce32e876869f13d553fefd
Robin Timmermans <r.h.timmermans@gmail.com>
2026-01-10 00:08:21 +0100

    birbs

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,9 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrength;
+        }
     }
 }
